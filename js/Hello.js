71498af50e1c1ebff63dda77115d9965cd5e896a
Satima Valsony <valsonysatima2002@gmail.com>
2022-03-13 10:00:00 +0200

    First commit

---
+++ js/Hello.js
@@ -0,0 +1,23 @@
+
+let person={
+	name: "Satima",
+	age: 28
+};
+
+let selectedColors=["red","green"];
+console.log(selectedColors);
+
+function sayHello(){
+	console.log("Hello!!");
+}
+const circle={
+	radius: 1,
+	location: {
+		x:1,
+		y:2
+	},
+	draw: function (){
+		console.log("draw circle");
+	}
+};
+circle.radius;
